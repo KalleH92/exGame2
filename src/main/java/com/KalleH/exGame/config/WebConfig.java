c16fca235cb39d4ff9e3128a7a2d4d964fd7e54f
KalleH92 <kalle.hagberg92@hotmail.com>
2024-12-30 17:28:44 +0100

    Commit linking several pages

--- src/main/java/com/KalleH/exGame/config/WebConfig.java
+++ src/main/java/com/KalleH/exGame/config/WebConfig.java
@@ -1,13 +1,13 @@
 package com.KalleH.exGame.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/main").setViewName("main");
-    }
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+public class WebConfig {
+
 }
--- src/main/java/com/KalleH/exGame/controller/GameController.java
+++ src/main/java/com/KalleH/exGame/controller/GameController.java
@@ -1,6 +1,5 @@
 package com.KalleH.exGame.controller;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -11,25 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.SQLOutput;
 
 @SpringBootApplication
-@RestController
 @Controller
 public class GameController {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
     }
-
-
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+     */
+    @GetMapping("/")
+    public String home() {
+        System.out.println("Homepage");
+        return "home";
+    }
 
-    @GetMapping("/index")
+    @GetMapping("/main")
     public String main() {
+        System.out.println("Main menu");
         return "main";
     }
 
+    @GetMapping("/stats")
+    public String stats() {
+        System.out.println("Stats");
+        return "stats";
+    }
+    @GetMapping("/game")
+    public String game() {
+        System.out.println("Game screen");
+        return "game";
+    }
 
 
 }
