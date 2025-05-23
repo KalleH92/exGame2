009d95baa300c035fa3b84bcaf7449e04f4e401c
KalleH92 <kalle.hagberg92@hotmail.com>
2025-05-23 14:17:45 +0200

    Add fixes to be able to test it and some other stuff

--- src/main/java/com/KalleH/exGame/ExGameApplication.java
+++ src/main/java/com/KalleH/exGame/ExGameApplication.java
@@ -3,8 +3,10 @@ package com.KalleH.exGame;
 import com.KalleH.exGame.controller.GameController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ExGameApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/KalleH/exGame/controller/GameController.java
+++ src/main/java/com/KalleH/exGame/controller/GameController.java
@@ -3,12 +3,14 @@ package com.KalleH.exGame.controller;
 import com.KalleH.exGame.games.ClickGameService;
 import com.KalleH.exGame.model.Player;
 import com.KalleH.exGame.Repository.PlayerRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GameController {
@@ -72,4 +74,39 @@ public class GameController {
         System.out.println("Game screen");
         return "game";
     }
+
+
+    @GetMapping("/setPoints")
+    public String setPoints(@RequestParam int value, HttpSession session) {
+        Player player = getCurrentPlayer(session);
+        player.setPts(value);
+        playerRepository.save(player);
+        return "redirect:/game";
+    }
+    @GetMapping("/setFactories")
+    public String setFactories(@RequestParam int value, HttpSession session) {
+        Player player = getCurrentPlayer(session);
+        player.setFactories(value);
+        playerRepository.save(player);
+        return "redirect:/game";
+    }
+    @GetMapping("/setWorkers")
+    public String setWorkers(@RequestParam int value, HttpSession session) {
+        Player player = getCurrentPlayer(session);
+        player.setWorkers(value);
+        playerRepository.save(player);
+        return "redirect:/game";
+    }
+
+    private Player getCurrentPlayer(HttpSession session) {
+        Long playerId = (Long) session.getAttribute("playerId");
+
+        if (playerId == null) {
+            playerId = 1L;
+            session.setAttribute("playerId",playerId);
+        }
+        return playerRepository.findById(playerId)
+                .orElseThrow(() -> new RuntimeException("Player not found with ID: "));
+    }
+
 }
